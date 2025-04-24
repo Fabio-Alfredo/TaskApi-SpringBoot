c6aa26830fe5b39d5f0c516ba81006bb91063a8d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-23 20:00:25 -0600

    add(UserService): add new method for find users by email :sparkles:

--- src/main/java/com/task/taskapi/service/contrat/UserService.java
+++ src/main/java/com/task/taskapi/service/contrat/UserService.java
@@ -1,8 +1,10 @@
 package com.task.taskapi.service.contrat;
 
 import com.task.taskapi.domain.dtos.RegisterDto;
+import com.task.taskapi.domain.models.User;
 
 public interface UserService {
     void createUser(RegisterDto userDto);
+    User findUserByEmail(String email);
 
 }
