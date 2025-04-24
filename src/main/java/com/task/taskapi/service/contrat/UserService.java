45b35f260b8cd8e6ef003c23873c842db49173b9
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-24 17:41:25 -0600

    add(UserServies): add new methos for validate o generate token :sparkles:

--- src/main/java/com/task/taskapi/service/contrat/UserService.java
+++ src/main/java/com/task/taskapi/service/contrat/UserService.java
@@ -1,10 +1,15 @@
 package com.task.taskapi.service.contrat;
 
 import com.task.taskapi.domain.dtos.RegisterDto;
+import com.task.taskapi.domain.models.Token;
 import com.task.taskapi.domain.models.User;
 
 public interface UserService {
     void createUser(RegisterDto userDto);
     User findUserByEmail(String email);
 
+    //Functions for token validations
+    Token registerToken(User user) throws Exception;
+    Boolean isTokenValid(User user, Token token) ;
+    void cleanToken(User user) throws Exception;
 }
