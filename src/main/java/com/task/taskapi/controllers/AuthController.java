2995ca7d68f6faffe434d5373795732d77ad7a7f
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-25 11:47:38 -0600

    add(AuthController): add new method for login user :sparkles:

--- src/main/java/com/task/taskapi/controllers/AuthController.java
+++ src/main/java/com/task/taskapi/controllers/AuthController.java
@@ -1,12 +1,19 @@
 package com.task.taskapi.controllers;
 
 import com.task.taskapi.domain.dtos.GeneralResponse;
+import com.task.taskapi.domain.dtos.LoginDto;
 import com.task.taskapi.domain.dtos.RegisterDto;
+import com.task.taskapi.domain.dtos.TokenDto;
+import com.task.taskapi.domain.models.Token;
+import com.task.taskapi.domain.models.User;
 import com.task.taskapi.service.contrat.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.naming.Binding;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -28,4 +35,21 @@ public class AuthController {
             return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
+
+    //Cambiar la logica para el servicio
+    @PostMapping("/login")
+    public  ResponseEntity<GeneralResponse> loginUser (@RequestBody @Valid LoginDto data, BindingResult validations){
+        try{
+            User user = userService.findUserByEmail(data.getEmail());
+            if(user == null ){
+                return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, "User not found");
+            }
+
+            Token token = userService.registerToken(user);
+            return GeneralResponse.getResponse(HttpStatus.OK, "User logged in successfully", new TokenDto(token) );
+        }catch (Exception e){
+            return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
 }
