13b13daf2ac4b452e3a5d7572762891e60ee7430
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-25 12:01:44 -0600

    add(JWTTokenFilter): add new filters for validation token by user :sparkles:

--- src/main/java/com/task/taskapi/utils/JWTTokenFilter.java
+++ src/main/java/com/task/taskapi/utils/JWTTokenFilter.java
@@ -1,2 +1,72 @@
-package com.task.taskapi.utils;public class JWTTokenFillter {
+package com.task.taskapi.utils;
+
+import com.task.taskapi.domain.models.User;
+import com.task.taskapi.service.contrat.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTTokenFilter extends OncePerRequestFilter {
+    private final JWTTools jwtTools;
+    private final UserService userService;
+
+    public JWTTokenFilter(JWTTools jwtTools, UserService userService) {
+        this.jwtTools = jwtTools;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenHeader = request.getHeader("Authorization");
+        String email = null;
+        String token = null;
+
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ") && tokenHeader.length() > 7) {
+            token = tokenHeader.substring(7);
+            try {
+                email = jwtTools.getUserByEmailFrom(token);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get JWT token");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT token has expired");
+            } catch (MalformedJwtException e) {
+                System.out.println("JWT token is malformed");
+            }
+        } else {
+            System.out.println("Bearer String not found");
+        }
+
+        if(email != null && token != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            User user = userService.findUserByEmail(email);
+
+            if(user != null){
+                Boolean tokenValidity = userService.isTokenValid(user, token);
+
+                if(tokenValidity){
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+
 }
