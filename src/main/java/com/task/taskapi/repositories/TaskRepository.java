503fd28975ae56546866a146365719c7347af5a0
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 16:44:00 -0600

    add(TaskRepository): add new method for find tasks by user :sparkles:

--- src/main/java/com/task/taskapi/repositories/TaskRepository.java
+++ src/main/java/com/task/taskapi/repositories/TaskRepository.java
@@ -1,11 +1,15 @@
 package com.task.taskapi.repositories;
 
+import com.task.taskapi.domain.dtos.task.ResponseTaskDto;
 import com.task.taskapi.domain.models.Task;
+import com.task.taskapi.domain.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<ResponseTaskDto> findAllByUser(User user);
 }
