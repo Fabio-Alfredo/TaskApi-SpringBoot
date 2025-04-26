1c8778ffaecb58d9fc322adaec8bca052dfd4bdc
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 11:57:20 -0600

    add(ResponseUserDto): add new dto for response data user :sparkles:

--- src/main/java/com/task/taskapi/domain/dtos/user/ResponseUserDto.java
+++ src/main/java/com/task/taskapi/domain/dtos/user/ResponseUserDto.java
@@ -1,2 +1,23 @@
-package com.task.taskapi.domain.dtos.user;public class ResponseUserDto {
+package com.task.taskapi.domain.dtos.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.task.taskapi.domain.models.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+public class ResponseUserDto {
+    private UUID id;
+    private String name;
+    private String email;
+
+    public ResponseUserDto(User user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
 }
