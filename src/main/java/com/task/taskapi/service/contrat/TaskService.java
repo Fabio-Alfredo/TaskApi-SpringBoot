ae92c89c33aba8efdf148147c901f2674b1bc652
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 11:54:36 -0600

    add(TaskService): add new methods for tasks :sparkles:

--- src/main/java/com/task/taskapi/service/contrat/TaskService.java
+++ src/main/java/com/task/taskapi/service/contrat/TaskService.java
@@ -1,2 +1,16 @@
-package com.task.taskapi.service.contrat;public interface TaskService {
+package com.task.taskapi.service.contrat;
+
+import com.task.taskapi.domain.dtos.task.CreateTaskDto;
+import com.task.taskapi.domain.models.Task;
+import com.task.taskapi.domain.models.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+    Task createTask(CreateTaskDto task, User user);
+    List<Task> getAllTasks();
+    List<Task>getAllTasksByUser(User user);
+    Task getTaskById(String id);
+
 }
