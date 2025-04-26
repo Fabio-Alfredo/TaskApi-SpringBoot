55e87e2fc76731f6fa96f439b4485ac744d66424
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 11:55:01 -0600

    add(TaskServiceImpl): add new implementation for create tasks :sparkles:

--- src/main/java/com/task/taskapi/service/impl/TaskServiceImpl.java
+++ src/main/java/com/task/taskapi/service/impl/TaskServiceImpl.java
@@ -1,2 +1,56 @@
-package com.task.taskapi.service.impl;public class TaskServiceImpl {
+package com.task.taskapi.service.impl;
+
+import com.task.taskapi.domain.dtos.task.CreateTaskDto;
+import com.task.taskapi.domain.models.Task;
+import com.task.taskapi.domain.models.User;
+import com.task.taskapi.repositories.TaskRepository;
+import com.task.taskapi.service.contrat.TaskService;
+import com.task.taskapi.service.contrat.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, UserService userService, ModelMapper modelMapper) {
+        this.taskRepository = taskRepository;
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public Task createTask(CreateTaskDto task, User user) {
+        try{
+            Task taskEntity = modelMapper.map(task, Task.class);
+            taskEntity.setUser(user);
+
+            return taskRepository.save(taskEntity);
+        }catch (Exception e){
+            throw new RuntimeException(
+                    e.getMessage() != null ? e.getMessage() : "Error while creating task "
+            );
+        }
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return List.of();
+    }
+
+    @Override
+    public List<Task> getAllTasksByUser(User user) {
+        return List.of();
+    }
+
+    @Override
+    public Task getTaskById(String id) {
+        return null;
+    }
 }
