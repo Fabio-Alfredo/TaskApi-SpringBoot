49409e698ffc4160602d9f2669ac29c7159ad105
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 17:34:24 -0600

    add(TaskService): add new method for update status in task :sparkles:

--- src/main/java/com/task/taskapi/service/contrat/TaskService.java
+++ src/main/java/com/task/taskapi/service/contrat/TaskService.java
@@ -2,7 +2,9 @@ package com.task.taskapi.service.contrat;
 
 import com.task.taskapi.domain.dtos.task.CreateTaskDto;
 import com.task.taskapi.domain.dtos.task.ResponseTaskDto;
+import com.task.taskapi.domain.dtos.task.UpdateTaskDto;
 import com.task.taskapi.domain.models.Task;
+import com.task.taskapi.domain.models.TaskStatus;
 import com.task.taskapi.domain.models.User;
 
 import java.util.List;
@@ -13,5 +15,6 @@ public interface TaskService {
     List<ResponseTaskDto> getAllTasks();
     List<ResponseTaskDto>getAllTasksByUser(User user);
     Task getTaskById(UUID id, User user);
+    Task updateStatusTask(UUID id, TaskStatus status, User user);
 
 }
