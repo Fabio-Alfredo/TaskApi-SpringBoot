068a9978e5e342df0854cdba540a2b986907788d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 11:58:31 -0600

    add(ResponseTaskDto): add new dto for response by find Tasks :sparkles:

--- src/main/java/com/task/taskapi/domain/dtos/task/ResponseTaskDto.java
+++ src/main/java/com/task/taskapi/domain/dtos/task/ResponseTaskDto.java
@@ -1,2 +1,27 @@
-package com.task.taskapi.domain.dtos.task;public class ResponseTaskDto {
+package com.task.taskapi.domain.dtos.task;
+
+import com.task.taskapi.domain.dtos.user.ResponseUserDto;
+import com.task.taskapi.domain.models.Task;
+import com.task.taskapi.domain.models.TaskStatus;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class ResponseTaskDto {
+    private UUID id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private ResponseUserDto user;
+
+    public ResponseTaskDto(Task task){
+        this.id = task.getId();
+        this.title = task.getTitle();
+        this.description = task.getDescription();
+        this.status = task.getStatus();
+        this.user = new ResponseUserDto(task.getUser());
+    }
 }
