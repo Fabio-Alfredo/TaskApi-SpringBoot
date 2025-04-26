769ff28a16be8938794ec39cd8e35e81f77cd6cb
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 17:33:49 -0600

    add: new dto for update status in task :sparkles:

---
+++ src/main/java/com/task/taskapi/domain/dtos/task/UpdateStatusTaskDto.java
@@ -0,0 +1,11 @@
+package com.task.taskapi.domain.dtos.task;
+
+import com.task.taskapi.domain.models.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UpdateStatusTaskDto {
+    @NotBlank(message = "Status is required")
+    private TaskStatus status;
+}
