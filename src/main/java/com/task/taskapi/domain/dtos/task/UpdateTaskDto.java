3e54b14c6293495cd661cb9162cacdade523978d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 19:23:57 -0600

    add(UpdateTaskDto): add new dto for update data by task :sparkles:

--- src/main/java/com/task/taskapi/domain/dtos/task/UpdateTaskDto.java
+++ src/main/java/com/task/taskapi/domain/dtos/task/UpdateTaskDto.java
@@ -1,2 +1,10 @@
-package com.task.taskapi.domain.dtos.task;public class UpdateTaskDto {
+package com.task.taskapi.domain.dtos.task;
+
+import com.task.taskapi.domain.models.TaskStatus;
+import lombok.Data;
+
+@Data
+public class UpdateTaskDto {
+    private String title;
+    private String description;
 }
