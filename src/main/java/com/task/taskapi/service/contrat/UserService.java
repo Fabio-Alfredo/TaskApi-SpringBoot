af8c63f9a48d5b83cfde16f43eec67aa56e4076e
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-26 21:33:36 -0600

    add(UserService): add new method for find users and udpate roles in user :sparkles:

--- src/main/java/com/task/taskapi/service/contrat/UserService.java
+++ src/main/java/com/task/taskapi/service/contrat/UserService.java
@@ -2,15 +2,21 @@ package com.task.taskapi.service.contrat;
 
 import com.task.taskapi.domain.dtos.auth.LoginDto;
 import com.task.taskapi.domain.dtos.auth.RegisterDto;
+import com.task.taskapi.domain.dtos.user.AddRoleDto;
+import com.task.taskapi.domain.dtos.user.ResponseUserDto;
 import com.task.taskapi.domain.models.Token;
 import com.task.taskapi.domain.models.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
     void createUser(RegisterDto userDto);
     User findUserByEmail(String email);
     User findById(UUID id);
+    List<ResponseUserDto>findAllUsers();
+    User updateRolesInUser(UUID userId, String newRole);
+
 
     //Function for login user
     Token loginUser(LoginDto data) throws Exception;
